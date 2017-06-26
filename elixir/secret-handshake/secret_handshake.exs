0176a078ab3327e66fc87eb03b508537007219e8
Marc Delagrammatikas <mdelagrammatikas@truecar.com>
2017-06-26 14:49:12 -0700

    Secret handshake solution.

--- elixir/secret-handshake/secret_handshake.exs
+++ elixir/secret-handshake/secret_handshake.exs
@@ -1,4 +1,13 @@
 defmodule SecretHandshake do
+  use Bitwise
+
+  @actions [
+    { 0b1, "wink" },
+    { 0b10, "double blink" },
+    { 0b100, "close your eyes" },
+    { 0b1000, "jump" }
+  ]
+
   @doc """
   Determine the actions of a secret handshake based on the binary
   representation of the given `code`.
@@ -15,6 +24,21 @@ defmodule SecretHandshake do
   """
   @spec commands(code :: integer) :: list(String.t())
   def commands(code) do
+    code
+    |> find_matching_actions
+    |> Enum.unzip
+    |> reverse_if_necessary(code)
+  end
+
+  def find_matching_actions(code) do
+      Enum.filter(@actions, fn {command, _} -> (code &&& command) > 0 end)
+  end
+
+  def reverse_if_necessary({_binary, actions}, code) do
+    if (code &&& 0b10000) > 0 do
+      Enum.reverse actions
+    else
+      actions
+    end
   end
 end
-
